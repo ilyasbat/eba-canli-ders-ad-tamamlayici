fb5c4f3bfd123dc037e512310bc9139d961fe507
İlyas BAT <ilyasbat76@gmail.com>
2020-11-05 13:54:44 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,36 +1,53 @@
-
-
 $(document).ready(()=>{
-
-        const newOgretmenler = ogretmenler.map(e=> {
-            return {
-                isim:e.isim.toLocaleLowerCase(),
-                id:e.id
-            }
-        });
-
+    chrome.storage.sync.get(["ebaData"], function(items){
+        const DERSLER = items.ebaData.split('\n');
+        const ogretmenler = [];
+        const newOgretmenler = [];
+        DERSLER.forEach((v,i)=>{
+            ogretmenler.push({id:i+1,isim:v.trim()})
+            newOgretmenler.push({id:i+1,isim:v.toLocaleLowerCase().trim()})
+        })
+        let index = -1;
         $(document).on('keyup', k => {
-
+            
 
             if(k.target.attributes["ng-model"].value==='render.etudName') {
-
-                let data = $('*[ng-model="render.etudName"]').val().toLocaleLowerCase();
+                const input = $('*[ng-model="render.etudName"]');
+                let data = input.val().toLocaleLowerCase();
                 if(data.length>0)
                 {
+            
+                    input.css('border-radius','0');
                     let searchData = newOgretmenler.filter(e=>e.isim.includes(data))
-                    let html = '<div style="margin:5px" id="onerilerIlyasBat">';
-                    searchData.forEach(v=>{
-                        let isim = ogretmenler.filter(e=>e.id===v.id);
-                        html += '<a href="javascript://"  class="ogretmenLinkIlyas">'+isim[0].isim+'</a><br/>'
-                    })
-                    html += '</div>';
-                    if($('#ilyasBatContainer').html()!==undefined){
-                        $('#ilyasBatContainer').html(html);
-                    }else{
-                        $('*[ng-model="render.etudName"]').after('<div id="ilyasBatContainer">'+html+'</div>');
+                    if(k.keyCode===40 && index<searchData.length-1){
+                        index += 1; 
+                    }
+                    if(k.keyCode===38 && index>-1){
+                        index -= 1; 
+                    }
+                    if(k.keyCode===13 && index>-1){
+                        $('.ogretmenLinkIlyas').eq(index).trigger('click');
+                    }
+                    if(k.keyCode!==13){
+                        let html = '<div  id="onerilerIlyasBat">';
+                        searchData.forEach((v,i)=>{
+                            let isim = ogretmenler.filter(e=>e.id===v.id);
+                            html += '<a href="javascript://"  class="ogretmenLinkIlyas '+(index===i?'secili':'')+'">'+isim[0].isim+'</a>'
+                        })
+                        html += '</div>';
+                        if($('#ilyasBatContainer').html()!==undefined){
+                            $('#ilyasBatContainer').html(html);
+                        }else{
+                            input.after('<div id="ilyasBatContainer">'+html+'</div>');
+                        }
+                    }
+                    else{
+                        input.css('border-radius','3px');
+                        $('#ilyasBatContainer').html('');
                     }
                 }
                 else{
+                    input.css('border-radius','3px');
                     $('#ilyasBatContainer').html('');
                 }
                 $('.ogretmenLinkIlyas').on('click',e=>{
@@ -41,8 +58,9 @@ $(document).ready(()=>{
                     document.execCommand('insertText', false, e.target.innerText);
                     el.dispatchEvent(new Event('change', {bubbles: true}));
                     $('#ilyasBatContainer').html('');
+                    index = -1;
                 })
             }
         })
-
+    })
 })
